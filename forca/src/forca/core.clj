fba0ef43d2228eedc6efe7ef7eebf7786916850f
Willian Yassunaka <willianhy@gmail.com>
2017-07-26 22:40:18 -0300

    Finalizando app

--- forca/src/forca/core.clj
+++ forca/src/forca/core.clj
@@ -2,23 +2,30 @@
   (:gen-class))
 
 (def total-de-vidas 6)
-
+(def palavra-secreta "MELANCIA")
 (defn perdeu [] (print "Você perdeu"))
 (defn ganhou [] (print "Você ganhou!"))
 
-(defn letras-faltantes [palavras acertos]
-	(remove (fn [letras] (contains? acertos (str letra))) palavra)
-)
+(defn letras-faltantes [palavra acertos]
+	(remove (fn [letra] (contains? acertos (str letra))) palavra))
 
 (defn acertou-a-palavra-toda? [palavra acertos]
-	(empty? (letras-faltantes palavra acertos))
-)
+	(empty? (letras-faltantes palavra acertos)))
 
 (defn le-letra! [] (read-line))
 
 (defn acertou? [chute palavra] (.contains palavra chute))
 
-(defn jogo [vidas]
+(defn imprime-forca [vidas palavra acertos]
+	(println "Vidas " vidas)
+	(doseq [letra (seq palavra)] 
+		(if (contains? acertos (str letra))
+			(print letra " ") 
+			(print "_" " ")))
+	(println))
+
+(defn jogo [vidas palavra acertos]
+	(imprime-forca vidas palavra acertos)
 	(cond
 			(= vidas 0) (perdeu)
 			(acertou-a-palavra-toda? palavra acertos) (ganhou)
@@ -33,10 +40,14 @@
 							(recur (dec vidas) palavra acertos))
 				)		
 			)
-	)
-)
+	))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;(->> carros
+;	(map (fn [x] (* x 2)))
+;	(map (fn [x] (* x 3)))
+;	(reduce (fn [acc n] (+ acc n))))
+
+(defn comeca-o-jogo [] (jogo total-de-vidas palavra-secreta #{}))
+
+(defn -main [& args]
+	(comeca-o-jogo))
